8bf447c92f72dab3b5fbe5caeefe40527907b08e
vishal <Vishalkumar3474@gmail.com>
2022-05-27 22:16:01 +0530

    r

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -7,7 +7,7 @@ const Resume = () => {
     <div name='Resume' className='w-full my-32'  >
      <h1 class="text-indigo-500 text-4xl font-bold text-center mb-6"  >Resume</h1>
      
-    <div class=" border border-2 border-indigo-400 mr-6 ml-6 ">
+    <div class=" border border-2 border-indigo-400 mr-20 ml-20 ">
     <img  src="https://cdn.discordapp.com/attachments/944189291427430421/979783635404206112/Vishal_InternshalaResume_2_page-0001.jpg"></img>
      </div>
     <div className='items-center text-center'>
