f475928e680938b6928f271105162ef3fa0b0b95
vishal <Vishalkumar3474@gmail.com>
2022-05-27 22:13:42 +0530

    ml mr

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -7,8 +7,8 @@ const Resume = () => {
     <div name='Resume' className='w-full my-32'  >
      <h1 class="text-indigo-500 text-4xl font-bold text-center mb-6"  >Resume</h1>
      
-    <div class=" border border-1 border-black ">
-    <img className='mr-4 ml-4 '  src="https://cdn.discordapp.com/attachments/944189291427430421/979783635404206112/Vishal_InternshalaResume_2_page-0001.jpg"></img>
+    <div class=" border border-2 border-indigo-400 mr-6 ml-6 ">
+    <img  src="https://cdn.discordapp.com/attachments/944189291427430421/979783635404206112/Vishal_InternshalaResume_2_page-0001.jpg"></img>
      </div>
     <div className='items-center text-center'>
     <button className='p-2 mb-4 mt-2 mb-2 text-center'><a href="https://drive.google.com/file/d/1fvADDl6MmPLGAKhw2lMH4eKE7DGqFSkC/view?usp=sharing">Download Resume</a></button>
