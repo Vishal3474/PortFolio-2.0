a8809f7bb358817fce66d84433dbe61b270d5409
vishal <Vishalkumar3474@gmail.com>
2022-05-27 23:37:58 +0530

    figma

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -86,7 +86,7 @@ const Projects = () => {
 "
               >
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  Figma Responsibe Page
+                  Responsibe_Page{" "}
                 </h5>
               </a>
               <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
