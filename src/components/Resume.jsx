4d67909beba88331819fb25eb99fe7bfa52fc296
vishal <Vishalkumar3474@gmail.com>
2022-05-27 22:02:26 +0530

    resume change

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -7,7 +7,7 @@ const Resume = () => {
     <div name='Resume' className='w-full my-32'  >
      <h1 class="text-indigo-500 text-4xl font-bold text-center"  >Resume</h1>
     <div class=" border border-1 border-black ">
-    <img  src="https://cdn.discordapp.com/attachments/944189291427430421/979619964443066388/Web_developer_Resume.jpg"></img>
+    <img  src="https://cdn.discordapp.com/attachments/944189291427430421/979783635404206112/Vishal_InternshalaResume_2_page-0001.jpg"></img>
      </div>
     <div className='items-center text-center'>
     <button className='p-2 mb-4 mt-2 mb-2 text-center'><a href="https://drive.google.com/file/d/1-Ems5JioDmkpgfgVomtU1KAVtjJnizWY/view?usp=sharing">Download Resume</a></button>
