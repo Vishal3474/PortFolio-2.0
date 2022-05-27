a589696d28dac1bf99cd9628b6e5938871d4e0d5
VISHAL <97962363+Vishal3474@users.noreply.github.com>
2022-05-27 11:10:31 +0530

    Update Pricing.jsx

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -7,7 +7,7 @@ const Pricing = () => {
     <div name='pricing' className='w-full my-32'  >
      <h1 class="text-indigo-500 text-4xl font-bold text-center"  >Resume</h1>
     <div class=" border border-1 border-black ">
-    <img  src="https://cdn.discordapp.com/attachments/944189291427430421/964783609896632320/demoResume.jpg"></img>
+    <img  src="https://cdn.discordapp.com/attachments/944189291427430421/979619964443066388/Web_developer_Resume.jpg"></img>
      </div>
     
     <div className='items-center text-center'>
