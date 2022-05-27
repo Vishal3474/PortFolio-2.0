3e4f196d8a811dbe405833a4cb945e7555484534
vishal <Vishalkumar3474@gmail.com>
2022-05-27 21:34:14 +0530

    resume pdf

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -9,7 +9,7 @@ const Resume = () => {
     <div class=" border border-1 border-black ">
     <img  src="https://cdn.discordapp.com/attachments/944189291427430421/979619964443066388/Web_developer_Resume.jpg"></img>
      </div>
-    
+    <embed src="./assets/Resume.pdf" type="application/pdf" />
     <div className='items-center text-center'>
     <button className='p-2 mb-4 mt-2 mb-2 text-center'><a href="https://drive.google.com/file/d/1-Ems5JioDmkpgfgVomtU1KAVtjJnizWY/view?usp=sharing">Download Resume</a></button>
     </div>
