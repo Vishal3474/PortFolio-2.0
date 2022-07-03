89006df4d4c326059813c2b6a736e00caf1b85fb
vishal <Vishalkumar3474@gmail.com>
2022-07-03 23:14:09 +0530

    bg size

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -6,7 +6,7 @@ const Front = () => {
   return (
     <div
       name="home"
-      className="w-full h-screen bg-zinc-200 flex flex-col justify-between bg-[url('https://cdn.discordapp.com/attachments/944189291427430421/993208131964370984/ezgif.com-gif-maker.gif')]"
+      className="w-full h-screen bg-zinc-200 flex flex-col justify-between bg-cover bg-[url('https://cdn.discordapp.com/attachments/944189291427430421/993208131964370984/ezgif.com-gif-maker.gif')]"
     >
       <div className="grid md:grid-cols-2 max-w-[1240px] m-auto">
         <div className="flex flex-col justify-center md:items-start w-full px-2 py-8">
