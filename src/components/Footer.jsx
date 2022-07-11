4ec31d412846d22d92425a1f38d4afc08d26241a
vishal <Vishalkumar3474@gmail.com>
2022-07-11 13:57:21 +0530

    spin without div

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -49,20 +49,40 @@ function Footer() {
       <div className="flex flex-col max-w-[1240px] px-2 py-4 mx-auto justify-between sm:flex-row text-center text-gray-500">
         <p className="py-4"> Made With ❤ at CodeYogi</p>
         <p className="py-4"> Copyright © 2022 Vishal All rights reserved</p>
-        <div className="flex justify-between sm:w-[300px] pt-4 text-2xl motion-safe:animate-spin">
-          <a href="https://www.facebook.com" target="_blank">
+        <div className="flex justify-between sm:w-[300px] pt-4 text-2xl ">
+          <a
+            className="motion-safe:animate-spin"
+            href="https://www.facebook.com"
+            target="_blank"
+          >
             <FaFacebook />
           </a>
-          <a href="https://www.instagram.com/its_vi.shal/" target="_blank">
+          <a
+            className="motion-safe:animate-spin"
+            href="https://www.instagram.com/its_vi.shal/"
+            target="_blank"
+          >
             <FaInstagram />
           </a>
-          <a href="https://twitter.com/Vishal3474" target="_blank">
+          <a
+            className="motion-safe:animate-spin"
+            href="https://twitter.com/Vishal3474"
+            target="_blank"
+          >
             <FaTwitter />
           </a>
-          <a href="https://www.twitch.tv/its_vishal" target="_blank">
+          <a
+            className="motion-safe:animate-spin"
+            href="https://www.twitch.tv/its_vishal"
+            target="_blank"
+          >
             <FaTwitch />
           </a>
-          <a href="https://github.com/Vishal3474" target="_blank">
+          <a
+            className="motion-safe:animate-spin"
+            href="https://github.com/Vishal3474"
+            target="_blank"
+          >
             <FaGithub />
           </a>
         </div>
