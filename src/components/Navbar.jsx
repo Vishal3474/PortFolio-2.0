15afbef567dc4f8e8a204994b0f3c8c45e7f43ef
VISHAL <97962363+Vishal3474@users.noreply.github.com>
2022-08-19 19:37:44 +0530

    Update Navbar.jsx
    last update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,16 +44,12 @@ const Navbar = () => {
                 Resume
               </Link>
             </li>
+   <li class="text-purple-500 font-bold font-mono">
+            
+            Call +91 9520630066
+            </li>
           </ul>
-   <a href="tel:+919520630066">
-                    <img
-                      className="mr-2 animate-bounce"
-                      alt="Call"
-                      src="https://cdn.discordapp.com/attachments/944189291427430421/987951071194402847/60-603850_call-us-now-button-camerabewaking-removebg-preview.png"
-                      width="100"
-                      height="50"
-                    />
-                  </a>
+  
 </div>   
         </div>
       </div>
