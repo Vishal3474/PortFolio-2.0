5d465a8e0bad4ce68cd6605e03a6ef70254ebe32
VISHAL <97962363+Vishal3474@users.noreply.github.com>
2022-08-19 18:29:37 +0530

    Update Footer.jsx
    Footer Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,14 +16,14 @@ function Footer() {
   };
 
   const BtnClick = () => {
-    alert("Subscribed SuccessFully!");
+    alert(" Message Send SuccessFully!");
   };
 
   return (
     <div className="w-full mt-24 bg-slate-900 text-gray-300  py-y px-2">
       <div className=" md:grid-cols-6 border-b-2 items-center border-gray-600">
         <div className="col-span-2 text-center items-center  md:pt-2">
-          <p className="font-bold uppercase">Subscribe to our newsletter</p>
+          <p className="font-bold uppercase">Hiring Me</p>
           <p className="py-4">
             The latest news, articles, and resources, sent to your inbox weekly.
           </p>
@@ -32,15 +32,15 @@ function Footer() {
               value={inputValue}
               onChange={inputChange}
               className="w-full p-2 mr-4 rounded-md mb-4"
-              type="email"
-              placeholder="Enter email.."
+              type="text"
+              placeholder="Enter Your Messages.."
             />
             <button
               disabled={inputValue.length < 1}
               onClick={BtnClick}
               className="p-2 mb-4 bg-indigo-700 disabled:bg-gray-700"
             >
-              Subscribe
+            Send Message
             </button>
           </form>
         </div>
@@ -49,13 +49,13 @@ function Footer() {
       <div className="flex flex-col max-w-[1240px] px-2 py-4 mx-auto justify-between sm:flex-row text-center text-gray-500">
         <p className="py-4">
           {" "}
-          Made With <span className="motion-safe:animate-spin">❤</span> at
+          Made With <span >❤</span> at
           CodeYogi
         </p>
         <p className="py-4"> Copyright © 2022 Vishal All rights reserved</p>
         <div className="flex justify-between sm:w-[300px] pt-4 text-2xl ">
           <a
-            className="motion-safe:animate-spin"
+            
             href="https://www.facebook.com"
             target="_blank"
           >
@@ -69,21 +69,21 @@ function Footer() {
             <FaInstagram />
           </a>
           <a
-            className="motion-safe:animate-spin"
+          
             href="https://twitter.com/Vishal3474"
             target="_blank"
           >
             <FaTwitter />
           </a>
           <a
-            className="motion-safe:animate-spin"
+          
             href="https://www.twitch.tv/its_vishal"
             target="_blank"
           >
             <FaTwitch />
           </a>
           <a
-            className="motion-safe:animate-spin"
+        
             href="https://github.com/Vishal3474"
             target="_blank"
           >
